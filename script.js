3906ce804efc97b50e420ae879431797b01a1ebd
Matheus André <122615835+mts-andre@users.noreply.github.com>
2023-04-05 14:49:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function verificarTamanhoTela() {
 
 verificarTamanhoTela()
 
+function posicionarElemento() {
+    var posX = Math.floor(Math.random() * lag) - 90
+    var posY = Math.floor(Math.random() * alt) - 90
+
+    posX = posX < 0 ? 0 : posX
+    posY = posY < 0 ? 0 : posY
+
+    var mosk = window.document.createElement('img')
+    mosk.src = 'img/mosca.png'
+    mosk.className = 'mosk'
+    mosk.style.left = posX + 'px'
+    mosk.style.top = posY + 'px'
+    mosk.style.position = 'absolute'
+
+    window.document.body.appendChild(mosk)
+}
